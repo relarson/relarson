ccec2d209700315b073abe4060d2a5c2fb9e4e59
Ross Larson <rlarson@factset.com>
2016-05-20 14:53:29 -0500

    add onload function

--- js/application.js
+++ js/application.js
@@ -1,8 +1,10 @@
-var players = [];
-var list = document.getElementsByClassName("player-list")[0];
+window.onload=function() {
+    var players = [];
+    var list = document.getElementsByClassName("player-list")[0];
 
-players.push(new Player("Ross"));
+    players.push(new Player("Ross"));
 
-players.forEach(function(player) {
-    list.appendChild(player.name);
-}, this);
+    players.forEach(function(player) {
+        list.appendChild(player.name);
+    }, this);
+}
