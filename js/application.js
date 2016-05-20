9f3419ab16c4ec1079e641353055771eabbd6c49
Ross Larson <rlarson@factset.com>
2016-05-20 14:51:33 -0500

    add new application.js file, try adding a player programmatically

---
+++ js/application.js
@@ -0,0 +1,8 @@
+var players = [];
+var list = document.getElementsByClassName("player-list")[0];
+
+players.push(new Player("Ross"));
+
+players.forEach(function(player) {
+    list.appendChild(player.name);
+}, this);
