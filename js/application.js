71a8e6ad0ba154d01d3479820dc11c5ad947c5af
Ross Larson <rlarson@factset.com>
2016-05-20 15:32:33 -0500

    add hidden button

--- js/application.js
+++ js/application.js
@@ -16,7 +16,7 @@ window.onload=function() {
 }
 
 var addPlayer = function() {
-    if (event.keyCode === 13 && list !== undefined) {
+    if (list !== undefined) {
         var player = new Player(document.getElementById("add-player").value);
         players.push(player);
         var item = document.createElement("li");
@@ -24,4 +24,15 @@ var addPlayer = function() {
         item.appendChild(name);
         list.appendChild(item);
     }
+}
+
+var searchKeyPress = function(e) {
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('add-player-btn').click();
+        return false;
+    }
+    return true;
 }
